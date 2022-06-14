8468eda84684fb86f7df765f4b1a1165e7a7fac7
elf <elorian@yandex.ru>
2022-06-14 23:16:28 +0300

    Start creating order model

--- self_storage_site/starage_manager/models.py
+++ self_storage_site/starage_manager/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 User = get_user_model()
 
-
+class Order (models.Model):
+    number = models.BigAutoField('Номер заказа')
+    customer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='orders')
+    box = ...
+    tariff = ...
+    start_date = models.DateTimeField('заказ от', auto_now_add=True)
+    key = models.PositiveIntegerField('Ключ доступа')
