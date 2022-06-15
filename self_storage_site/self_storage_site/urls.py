f84fe8046078eb23af8041fe0f91188821fb3e3f
pkzrnvch <pkazarnovich@gmail.com>
2022-06-16 02:58:47 +0500

    Add static files and templates

--- self_storage_site/self_storage_site/urls.py
+++ self_storage_site/self_storage_site/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import render
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('', render, kwargs={'template_name': 'index.html'}, name='start_page'),
+    path('faq/', render, kwargs={'template_name': 'faq.html'}, name='faq'),
+    path('register/', include('storage_manager.urls')),
     path('admin/', admin.site.urls),
 ]
