ef603dc93987f319937108d9d77f32a30e9ceae1
pkzrnvch <pkazarnovich@gmail.com>
2022-06-17 10:12:12 +0500

    Enable logout button

--- self_storage_site/self_storage_site/settings.py
+++ self_storage_site/self_storage_site/settings.py
@@ -140,6 +140,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- self_storage_site/storage_manager/urls.py
+++ self_storage_site/storage_manager/urls.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path('users_prof/', render, kwargs={'template_name': 'my-rent.html'}, name='profile'),
     path('register/', views.register, name='register'),
     path('login/', views.sign_in, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
