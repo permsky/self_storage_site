b0127038a679d2ba0bbf2ea2e0c0a51717c8cfc8
Boyko Alexey <elorian@yandex.ru>
2022-06-17 11:42:09 +0300

    Correct freespace logic in index view

--- self_storage_site/storage_manager/views.py
+++ self_storage_site/storage_manager/views.py
@@ -53,18 +53,18 @@ def sign_in(request):
 
 def index(request):
     #print(request.__dict__)
-    all_box_spaces = list(BoxPlace.objects.all().get_free_boxes().get_min_price())
+    all_box_spaces = list(BoxPlace.objects.all().prefetch_related(
+        'place_boxes').get_free_boxes().get_min_price())
     space_count = len(all_box_spaces)
     random_box_space_index = randomise_from_range(space_count)
     random_box_space = all_box_spaces[random_box_space_index]
-    #q = BoxPlace.objects.all().get_free_boxes()
-    #print(random_box_space.min_price)
+    free_space = random_box_space.boxes_quantity - random_box_space.free_boxes
 
     min_box_price = Box.objects.all().aggregate(Min('tariff'))['tariff__min']
     context = {
         'min_box_price': min_box_price,
         'box_space': random_box_space,
-        'free_space': random_box_space.free_boxes,
+        'free_space': free_space,
         'min_price': random_box_space.min_price,
     }
     return render(request, 'index.html', context)
